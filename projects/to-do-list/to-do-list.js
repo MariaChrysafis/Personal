2e3cc36886e678051c008f40d600c23d7c18358c
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2022-10-22 17:38:10 -0700

    to-do stuff
    to-do stuff

--- projects/to-do-list/to-do-list.js
+++ projects/to-do-list/to-do-list.js
@@ -1,4 +1,28 @@
-async function parse () {
-    x = document.getElementById("add_to_do").value;
-    document.getElementById("lister").innerHTML += x + '\r';
+var i = 0;
+async function typeSentence(txt, delay) {
+    for (var i = 0; i < txt.length; i++) {
+        await waitForMs(delay);
+        document.getElementById("demo").innerHTML += txt[i];
+    }
+    return;
 }
+  
+  
+function waitForMs(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+class ToDo {
+    constructor (assignment) {
+        this.assignment = assignment;
+    }
+}
+
+to_do_array = new Array();
+
+function addToDo () {
+    assignment = document.getElementById("iitem").value
+    document.getElementById("to-do").innerHTML += assignment;
+}
+
+typeSentence('A to-do list with competitive programming in mind!', 1)
