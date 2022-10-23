309974359dbc463d4dd47624892e3fdc718aaa13
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2022-10-22 22:31:48 -0700

    delete transitions
    delete transitions

--- projects/to-do-list/to-do-list.js
+++ projects/to-do-list/to-do-list.js
@@ -1,7 +1,7 @@
-async function typeSentence(txt, delay) {
+async function typeSentence(txt, delay, loc) {
     for (var i = 0; i < txt.length; i++) {
         await waitForMs(delay);
-        document.getElementById("demo").innerHTML += txt[i];
+        document.getElementById(loc).innerHTML += txt[i];
     }
     return;
 }
@@ -11,8 +11,13 @@ function waitForMs(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-function f () {
-    document.getElementById("demo").innerHTML += "hehe";
+function removeFadeOut( el, speed ) {
+    var seconds = speed/1000;
+    el.style.transition = "opacity "+seconds+"s ease";
+    el.style.opacity = 0;
+    setTimeout(function() {
+        el.parentNode.removeChild(el);
+    }, speed);
 }
 
 async function addToDo () {
@@ -20,14 +25,17 @@ async function addToDo () {
     var li = document.createElement("li");
     li.classList="animate1";
     li.onclick = function () {
-        this.parentElement.removeChild(this);
+        removeFadeOut(this, 1000);
     };
     str = document.getElementById("iitem").value;
     li.appendChild(document.createTextNode(str));
     ul.appendChild(li);
+    document.getElementById("iitem").value = "";
+
 }
 
-typeSentence('A to-do list!', 50)
+typeSentence('A to-do list!', 50, "demo")
+typeSentence('Click to delete terms', 50, "header")
 
 var close = document.getElementsByClassName("animate1");
 var i;
